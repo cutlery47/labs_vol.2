3c8443709b7d2c95a8440a51562973ef76a82eff
Arkhip McNutt <96910325+cutlery47@users.noreply.github.com>
2023-03-11 08:49:18 +0300

    Add files via upload

--- diff/diff.cpp
+++ diff/diff.cpp
@@ -9,12 +9,19 @@ int main(int argc, char* argv[]) {
     system("cls");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    // HideCursor();
+    HideCursor();
 
     char el;
-    int cntr = 0;
+    int cntr = 0, error = 0;
     int length_1 = 0, length_2 = 0;
     fstream file;
+
+    error = InputCheck(argc, argv);
+
+    if (error == 1) {
+        system("cls");
+        return 0;
+    }
 
     length_1 = GetFileLen(argv[1], length_1);
     length_2 = GetFileLen(argv[2], length_2);
--- diff/diff.h
+++ diff/diff.h
@@ -31,34 +31,6 @@ void SetCursor(int x, int y) { // sets the cursor in a particular position
 
 }
 
-void color(int x) {
-    if (x >= 0 && x <= 15)
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), x);
-    else
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);  
-} 
-
-char* ReverseString(const char* string, int len) {
-    char* rev_string = new char[len];
-
-    for (int i = 0; i < len; ++i) {
-        rev_string[i] = string[len - i - 1];
-    }
-
-    return rev_string;
-}
-
-char* ReverseString(char* string, int len) {
-    char* rev_string = new char[len];
-
-    for (int i = 0; i < len; ++i) {
-        rev_string[i] = string[len - i - 1];
-        // cout << rev_string[i];
-    }
-
-    return rev_string;
-}
-
 void PrintCenteredText(const char* string, int y, int multiplier) { // makes the text centered
     int i = 0, len = 0;
     while (string[i] != '\0') { // calculating string length
@@ -86,6 +58,42 @@ void Confirm(int y) {
     while (confirm != 13) {
         confirm = getch();
     }
+}
+
+int InputCheck (int argc, char** argv) {
+    if (argc < 3) {
+        PrintCenteredText("Недостаточное число аргументов...", 0, 1);
+        Confirm(1);
+        return 1;
+    }
+}
+
+void color(int x) {
+    if (x >= 0 && x <= 15)
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), x);
+    else
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);  
+} 
+
+char* ReverseString(const char* string, int len) {
+    char* rev_string = new char[len];
+
+    for (int i = 0; i < len; ++i) {
+        rev_string[i] = string[len - i - 1];
+    }
+
+    return rev_string;
+}
+
+char* ReverseString(char* string, int len) {
+    char* rev_string = new char[len];
+
+    for (int i = 0; i < len; ++i) {
+        rev_string[i] = string[len - i - 1];
+        // cout << rev_string[i];
+    }
+
+    return rev_string;
 }
 
 int GetFileLen(char* file_name, int len) {
@@ -221,16 +229,15 @@ void HighlightedPrint(char* common, const char* string, int string_size, int com
     }
 }
 
-
 void MainMenu(char* file_name_1, char* file_name_2, char* string_1, char* string_2, char* common, int string_1_size, int string_2_size, int common_size) { // main menu interface
     int current_line = 0;
     while (current_line < terminal_height) {
 
-        if (current_line == 0 || current_line == 2 || current_line == 5 || current_line == 20) {
+        if (current_line == 0 || current_line == 2 || current_line == 5 || current_line == terminal_height - 2) {
             cout << separator_line << endl;
         } else if (current_line == 1) {
             PrintCenteredText("Добро пожаловать в Beyond Compare 2", current_line, 1);
-        } else if ((current_line > 2 && current_line < 5) || (current_line > 5 && current_line < 20)) {
+        } else if ((current_line > 2 && current_line < 5) || (current_line > 5 && current_line < terminal_height - 3)) {
             if (current_line == 3) {
                 PrintCenteredText("Файл №1", current_line, -2);
                 PrintCenteredText("Файл №2", current_line, 2);
@@ -244,7 +251,7 @@ void MainMenu(char* file_name_1, char* file_name_2, char* string_1, char* string
             }
             PrintCenteredText("|||", current_line, 1);
             
-        } else if (current_line == 21) {
+        } else if (current_line == terminal_height - 1) {
             Confirm(current_line);
         }
         
